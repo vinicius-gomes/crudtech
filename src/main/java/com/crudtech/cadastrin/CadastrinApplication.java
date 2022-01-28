f51b45824f80323d961b7c2d4bc3e855fa99c599
Vinicius Augusto Gomes Felipes <vinicius.felipes@b2wdigital.com>
2022-01-28 12:24:36 -0300

    initial commit

---
+++ src/main/java/com/crudtech/cadastrin/CadastrinApplication.java
@@ -0,0 +1,13 @@
+package com.crudtech.cadastrin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrinApplication.class, args);
+	}
+
+}
