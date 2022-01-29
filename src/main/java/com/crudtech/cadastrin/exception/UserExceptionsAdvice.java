c343335868d0917fc8e78f5e69587fc8accb49b2
Vinicius Augusto Gomes Felipes <vinicius.felipes@b2wdigital.com>
2022-01-29 01:20:54 -0300

    move exception handler

--- src/main/java/com/crudtech/cadastrin/exception/UserExceptionsAdvice.java
+++ src/main/java/com/crudtech/cadastrin/exception/UserExceptionsAdvice.java
@@ -1,7 +1,5 @@
-package com.crudtech.cadastrin.controller;
+package com.crudtech.cadastrin.exception;
 
-import com.crudtech.cadastrin.exception.UserNotFoundException;
-import com.crudtech.cadastrin.exception.UserValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
